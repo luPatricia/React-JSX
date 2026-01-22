0960ea9befcb3f93153d473368e822da83317035
Patrícia Cardoso <luisacardosolu516@gmail.com>
2026-01-22 19:34:08 +0100

    primeiro commit de React

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+
+
+
+
+
+function App() {
+  return (
+    <main>
+       <header>
+          <img src="/logo.png" alt="" />
+       </header>
+
+       <section>
+             <img src="/banner.png" alt="" />
+       </section>
+    </main>
+  )
+}
+
+export default App
