473480475fc96c9fb11870b21fd31385fee19abe
Patrícia Cardoso <luisacardosolu516@gmail.com>
2026-01-23 02:20:14 +0100

    Criação do componente banner e do componente Temas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,79 @@
 
 import './App.css'
+import { Banner } from './componentes/Banner'
 import {FormularioDeEvento} from './componentes/FormularioDeEvento'
+import { Tema } from './componentes/Tema'
 
 
 function App() {
+ const temas = [
+   {
+    id: 1,
+    nome: 'front-end'
+   },
+
+   {
+    id: 2,
+    nome: 'back-end'
+   },
+
+   {
+    id: 3,
+    nome: 'devops'
+   },
+
+   {
+    id: 4,
+    nome: 'inteligência artificial'
+   },
+
+   {
+    id: 5,
+    nome: 'data science'
+   },
+
+   {
+    id: 6,
+    nome: 'cloud'
+   },
+
+ 
+ ]
+   
+ 
+
+
   return (
     <main>
        <header>
           <img src="/logo.png" alt="" />
        </header>
-
+      
+      <Banner/>
+       <FormularioDeEvento/>
        <section>
-             <img src="/banner.png" alt="" />
+        <Tema tema = {temas[0]}/>
        </section>
 
-       <FormularioDeEvento/>
+        <section>
+        <Tema tema = {temas[1]}/>
+       </section>
+
+        <section>
+        <Tema tema = {temas[2]}/>
+       </section>
+
+        <section>
+        <Tema tema = {temas[3]}/>
+       </section>
+
+        <section>
+        <Tema tema = {temas[4]}/>
+       </section>
+
+        <section>
+        <Tema tema = {temas[5]}/>
+       </section>
     </main>
   )
 }
---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,10 @@
+import './banner.css';
+
+export function Banner() {
+    return (
+        <section className='banner'>
+            <img src="/banner.png" alt="" />
+        </section>
+    )
+
+}
