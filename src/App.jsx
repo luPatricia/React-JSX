6c69669a47d626ac50b8ff3ac330706a5ee307c3
Patrícia Cardoso <luisacardosolu516@gmail.com>
2026-01-25 09:46:10 +0100

    criação do elemento Typography

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { Aside } from './components/Aside'
 import { Container } from './components/Container'
 import { Main } from './components/Main'
 import { SearchInput } from './components/SearchInput'
- 
+import { Typography } from './components/Typography'
+
 
 function App() {
-  
+
 
   return (
     <Container>
-            <Aside/>
-            <Main>
-                 <SearchInput/>
-            </Main>
+      <Aside />
+      <Main>
+        <SearchInput />
+        <div>
+           <Typography variant="h1">
+               Olá, Vinny!
+           </Typography>
+
+          <Typography variant="body">
+            Veja como estão suas finanças hoje.
+          </Typography>
+        </div>
+
+
+      </Main>
     </Container>
   )
 }
