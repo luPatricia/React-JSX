66782c4d6d3206b18dd1de42ef8bb52322c446ed
Patrícia Cardoso <luisacardosolu516@gmail.com>
2026-01-25 20:15:08 +0100

    implementação do Tailwind no projecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.svg'
+
+
+function App() {
+ 
+
+  return (
+    <div className="bg-neutral-background min-h-screen">
+       <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default App
