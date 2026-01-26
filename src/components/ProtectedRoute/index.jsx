7c66c9421202036e8afab6237dceb6e748ae0668
Patrícia Cardoso <luisacardosolu516@gmail.com>
2026-01-26 17:00:20 +0100

    Criação da rota protegida

---
+++ src/components/ProtectedRoute/index.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+import { useAuth } from "../../hooks/useAuth"
+import { Spinner } from "../Spinner"
+import { useNavigate } from "react-router"
+
+export const ProtectedRoute = ({children}) =>{
+ 
+  const {isAuthenticated, isLoading} =  useAuth()
+  const navigate = useNavigate()
+  
+  useEffect(() =>{
+     
+   if(!isLoading && !isAuthenticated){
+     navigate('/auth/login')
+    // redirect /auth/login
+   }
+
+  }, [isAuthenticated, isLoading] )
+
+
+  if(isLoading){
+     return <Spinner/>
+  }
+
+  if(isAuthenticated){
+    return null
+  }
+
+
+ return children
+}
